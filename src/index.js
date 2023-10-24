83bf5ff2533c2634ee83c7c3e858bda563d1d7b7
Constance Mokalapa <117752064+Constancemok4@users.noreply.github.com>
2023-10-24 21:00:34 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+function setDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayInput = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayInput];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchNow(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = setDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchNow);
